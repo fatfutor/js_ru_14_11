2ae6bc544c66c21e10a97d85c2c532fcfc54889d
Murat Barkinkhoev <murat-bark@yandex.ru>
2016-11-18 13:47:06 +0300

    Выполнено HT_1

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,30 +1,65 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Article extends Component {
+class Article extends React.Component {
 
     constructor() {
-        super()
+        super();
         this.state = {
-            isOpen: false,
-            obj: { foo: 'bar' }
+            isOpenArticle: false,
+            isOpenComments: false // как правильно ее "обзывать" существительное или глагол?
         }
     }
 
+    handleClick = evt => {
+        this.setState({
+            isOpenArticle: !this.state.isOpenArticle,
+            isOpenComments: false
+        })
+    };
+
+    isVisibleComments = evt => {
+        this.setState({
+            isOpenComments: !this.state.isOpenComments
+        })
+    };
+
     render() {
-        const { article } = this.props
-        const body = this.state.isOpen ? <p>{article.text}</p> : null
+        const { article } = this.props;
+        const body = this.state.isOpenArticle ?
+            <div>
+                <p>{article.text}</p>
+                <button onClick={this.isVisibleComments} disabled={(article.comments) ? '' : 'disabled'}>
+                    {
+                        (article.comments) ? (this.state.isOpenComments) ? 'закрыть' : 'открыть' : 'нет комментареив'
+                    }
+                </button>
+            </div>
+            : null;
+
+        let comments = null;
+
+        if (article.comments && this.state.isOpenComments) {
+            comments = article.comments.map((comments) => {
+                let title = (comments.title) ? comments.title : 'Без заголовка';
+                return (
+                    <li key={comments.id}>
+                        <h4>{title}</h4>
+                        <p>{comments.text}</p>
+                        <i>пользователь: {comments.user}</i>
+                    </li>
+                )
+            });
+        }
+
         return (
             <section>
                 <h3 onClick = {this.handleClick}>{article.title}</h3>
                 {body}
+                <ul>
+                    {comments}
+                </ul>
             </section>
         )
-    }
-
-    handleClick = ev => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
     }
 }
 
