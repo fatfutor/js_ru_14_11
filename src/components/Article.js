781d239e8027348cacfaa906dd32ceb0b0030eaa
Roman <kaboom.ua@gmail.com>
2016-11-21 11:28:54 +0200

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,6 +6,7 @@ class Article extends React.Component {
         super();
         this.state = {
             isOpenArticle: false,
+            //существительное, но лучше вынести этот стейт в отдельный компонент CommentList, иначе очень перегруженные компоненты выходят
             isOpenComments: false // как правильно ее "обзывать" существительное или глагол?
         }
     }
@@ -29,7 +30,7 @@ class Article extends React.Component {
             <div>
                 <p>{article.text}</p>
                 <button onClick={this.isVisibleComments} disabled={(article.comments) ? '' : 'disabled'}>
-                    {
+                    { /*не пиши сложный код в JSX, вынеси это в метод отдельный*/
                         (article.comments) ? (this.state.isOpenComments) ? 'закрыть' : 'открыть' : 'нет комментареив'
                     }
                 </button>
@@ -37,7 +38,7 @@ class Article extends React.Component {
             : null;
 
         let comments = null;
-
+        //Аналогично, видишь много кода - выноси в отдельный компонент
         if (article.comments && this.state.isOpenComments) {
             comments = article.comments.map((comments) => {
                 let title = (comments.title) ? comments.title : 'Без заголовка';
